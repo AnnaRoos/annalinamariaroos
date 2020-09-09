d68ee1044eb14fd4b2a93f3a7223abde655c0fe7
Anna Roos <anna.flytstrom@gmail.com>
2020-09-09 22:31:17 +0200

    added mute-btn, text-decor to link

--- My website/parallax.js
+++ My website/parallax.js
@@ -4,7 +4,7 @@ const targets = document.querySelectorAll('.show-on-scroll');
 const options = {
     root: null,
     rootMargin: '0px',
-    threshold: 0
+    threshold: 0.7
   }
 
 let observer = new IntersectionObserver(
@@ -20,13 +20,17 @@ targets.forEach(target => {
 });
 
 const twinkle = document.getElementById('twinkle');
+const muteBtn = document.getElementById('mute-btn');
 
 function unmute() {
     if(twinkle.muted) {
         twinkle.muted = false;
+        muteBtn.classList.add('mobile');
     } else {
         twinkle.muted = true;
+        muteBtn.classList.remove('mobile');
     }
 }
 
 twinkle.addEventListener('click', unmute);
+muteBtn.addEventListener('click', unmute);
